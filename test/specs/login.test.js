59df68ff460e83185215904fa9dd92144f7f5a8c
Renan Ferreira <renan.ferrer@hotmail.com>
2025-06-03 08:31:46 -0300

    Atualizado

--- test/specs/login.test.js
+++ test/specs/login.test.js
@@ -1,15 +1,8 @@
-import { expect } from '@wdio/globals'
-import LoginPage from '../pageobjects/login.page.js'
-import SecurePage from '../pageobjects/secure.page.js'
+import { expect, driver } from '@wdio/globals'
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-        await LoginPage.open()
-
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveText(
-            expect.stringContaining('You logged into a secure area!'))
+        await driver.pause(10000)
     })
 })
 
