ee770aa1325cc08be233c37728f537ee0a8a1c95
Renan Ferreira <renan.ferrer@hotmail.com>
2025-06-05 20:15:30 -0300

    devicefarm

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,22 +1,41 @@
 export const config = {
-    runner: 'local',
-    port: 4723,
-    
+    //runner: 'local',
+    //port: 4723,
+    user: 'oauth-yassu974-87197',
+    key: '994af757-4f59-4b1d-addf-703cbf7ba1b1',
+    hostname: 'ondemand.us-west-1.saucelabs.com',
+    port: 443,
+    baseUrl: 'wd/hub',
+
     specs: [
         './test/specs/**/*.js'
     ],
     
     maxInstances: 1,
 
-    capabilities: [{
-        platformName: 'Android',
-        'appium:deviceName': 'ebac',
-        'appium:platformVersion': '9.0',
-        'appium:automationName': 'UiAutomator2',
-        'appium:app': `${process.cwd()}/app/ebacshop/universal.apk`,
-        'appium:appWaitActivity': '.MainActivity',
-        'appium:disableIdLocatorAutocompletion': true,
-    }],
+    capabilities: [
+        //  {
+        //    platformName: 'Android',
+        //    'appium:deviceName': 'ebac',
+        //    'appium:platformVersion': '9.0',
+        //    'appium:automationName': 'UiAutomator2',
+        //    'appium:app': `${process.cwd()}/app/ebacshop/universal.apk`,
+        //    'appium:appWaitActivity': '.MainActivity',
+        //    'appium:disableIdLocatorAutocompletion': true,
+        //  }
+        {
+            platformName: 'Android',
+            'appium:app': 'storage:filename=ebacshop.aab',
+            'appium:deviceName': 'ebac',
+            'appium:platformVersion': '9.0',
+            'appium:automationName': 'UiAutomator2',
+            'sauce:options': {
+                build: 'appium-build-teste-ebacshop',
+                name: '<your test name>',
+                deviceOrientation: 'PORTRAIT',
+            },
+        }
+],
 
     logLevel: 'info',
     
