109bf37d4b874fbdd627dfaeb3437d2e0db80510
Renan Ferreira <renan.ferrer@hotmail.com>
2025-12-31 16:01:27 -0300

    chore: improve HomePage sync and mobile click handling

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -1,12 +1,28 @@
 class HomePage {
-    get profileTab() {
-        return $('id=tab-profile');
-    }
+  get profileTab() {
+    return $('id=tab-profile')
+  }
 
-    async goToProfile() {
-        await this.profileTab.waitForDisplayed({ timeout: 20000 });
-        await this.profileTab.click();
+  async goToProfile() {
+    await this.profileTab.waitForExist({
+      timeout: 40000,
+      timeoutMsg: 'Profile tab not found in DOM on Home screen'
+    })
+
+    await this.profileTab.waitForDisplayed({
+      timeout: 40000,
+      timeoutMsg: 'Profile tab not visible on Home screen'
+    })
+
+    try {
+      await this.profileTab.click()
+    } catch (error) {
+      await driver.touchAction({
+        action: 'tap',
+        element: this.profileTab
+      })
     }
+  }
 }
 
 export default new HomePage()
